d1e6659f67b27ce5167d4b512c9a7733120814cb
tse018 <60438717+tse018@users.noreply.github.com>
2021-11-23 12:35:44 +0100

    working on contactForm

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,12 +1,15 @@
 <template>
    <RouterView />
+   <ContactForm />
 </template>
 
 <script>
 
+import ContactForm from '../components/ContactForm.vue';
+
    export default {
       components: {
-
+         ContactForm
       },
    };
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+
+   <h1>Hei</h1>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style>
+</style>
