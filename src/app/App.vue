df9db19ad753a2ce6982aebf3e45972cb3fc9e6c
Dines <dinesvlarsen@gmail.com>
2021-11-27 16:01:42 +0100

    importing Quizapp from components, and inserting it into the dom, also prettier destroyed some indentation

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,21 +1,23 @@
 <template>
-   <RouterView />
-   <ContactForm />
+	<RouterView />
+	<ContactForm />
+	<Quizapp />
 </template>
 
 <script>
-
 import ContactForm from '../components/ContactForm.vue';
+import Quizapp from '../components/Quizapp.vue';
 
-   export default {
-      components: {
-         ContactForm
-      },
-   };
+export default {
+	components: {
+		ContactForm,
+		Quizapp,
+	},
+};
 </script>
 
 <style>
-   @import '../style/reset.css';
-   @import '../style/variables.css';
-   @import '../style/style.css';
+@import '../style/reset.css';
+@import '../style/variables.css';
+@import '../style/style.css';
 </style>
