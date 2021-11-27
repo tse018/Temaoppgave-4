569f2c369b378dffc08200d7f6cacc6c1d7bdb0f
Dines <dinesvlarsen@gmail.com>
2021-11-27 16:03:18 +0100

    prettier did some format magic

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,183 +1,218 @@
 <template>
-   <form>
-      <fieldset class="fieldset">
+	<form>
+		<fieldset class="fieldset">
+			<legend class="legend">Innmeldingsskjema</legend>
 
-         <legend class="legend">
-            Innmeldingsskjema
-         </legend>
+			<input
+				type="text"
+				name="firstname"
+				id="firstame"
+				placeholder="First name"
+				v-model="form.firstName"
+			/>
 
-         <input type="text" name="firstname" id="firstame" placeholder="First name"  v-model="form.firstName" />
+			<input
+				type="text"
+				name="lastname"
+				id="lastname"
+				placeholder="Last name"
+				v-model="form.lastName"
+			/>
 
-         <input type="text" name="lastname" id="lastname" placeholder="Last name"  v-model="form.lastName" />
+			<input
+				type="text"
+				name="adress"
+				id="address"
+				placeholder="Adress"
+				v-model="form.address"
+			/>
 
-         <input type="text" name="adress" id="address" placeholder="Adress"  v-model="form.address" />
+			<input
+				type="text"
+				name="postadress"
+				id="postadress"
+				placeholder="Postadress"
+				v-model="form.postAdress"
+			/>
 
-         <input type="text" name="postadress" id="postadress" placeholder="Postadress" v-model="form.postAdress" />
+			<input
+				type="text"
+				name="phonenumber"
+				id="phonenumber"
+				placeholder="Phonenumber"
+				v-model="form.phoneNumber"
+			/>
 
-         <input type="text" name="phonenumber" id="phonenumber" placeholder="Phonenumber" v-model="form.phoneNumber" />
+			<input
+				type="number"
+				name="email"
+				id="email"
+				placeholder="E-mail"
+				v-model="form.email"
+			/>
 
-         <input type="number" name="email" id="email" placeholder="E-mail" v-model="form.email" />
+			<label> Fødselsdato </label>
 
-         <label>
-            Fødselsdato
-         </label>
+			<select class="select-birthday" name="date" id="birthday">
+				<option value="--">--</option>
+				<option value="1">01</option>
+				<option value="2">02</option>
+				<option value="3">03</option>
+				<option value="4">04</option>
+				<option value="5">05</option>
+				<option value="6">06</option>
+				<option value="7">07</option>
+				<option value="8">08</option>
+				<option value="9">09</option>
+				<option value="10">10</option>
+				<option value="11">11</option>
+				<option value="12">12</option>
+				<option value="13">13</option>
+				<option value="14">14</option>
+				<option value="15">15</option>
+				<option value="16">16</option>
+				<option value="17">17</option>
+				<option value="18">18</option>
+				<option value="19">19</option>
+				<option value="20">20</option>
+				<option value="21">21</option>
+				<option value="22">22</option>
+				<option value="23">23</option>
+				<option value="24">24</option>
+				<option value="25">25</option>
+				<option value="26">26</option>
+				<option value="27">27</option>
+				<option value="28">28</option>
+				<option value="29">29</option>
+				<option value="30">30</option>
+				<option value="31">31</option>
+			</select>
 
-         <select class="select-birthday" name="date" id="birthday">
-            <option value="--">--</option>
-            <option value="1">01</option>
-            <option value="2">02</option>
-            <option value="3">03</option>
-            <option value="4">04</option>
-            <option value="5">05</option>
-            <option value="6">06</option>
-            <option value="7">07</option>
-            <option value="8">08</option>
-            <option value="9">09</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
-            <option value="24">24</option>
-            <option value="25">25</option>
-            <option value="26">26</option>
-            <option value="27">27</option>
-            <option value="28">28</option>
-            <option value="29">29</option>
-            <option value="30">30</option>
-            <option value="31">31</option>
-         </select>        
+			<select class="select-birthday" name="month" id="birthday">
+				<option value="--">--</option>
+				<option value="1">01</option>
+				<option value="2">02</option>
+				<option value="3">03</option>
+				<option value="4">04</option>
+				<option value="5">05</option>
+				<option value="6">06</option>
+				<option value="7">07</option>
+				<option value="8">08</option>
+				<option value="9">09</option>
+				<option value="10">10</option>
+				<option value="11">11</option>
+				<option value="12">12</option>
+			</select>
 
-         <select class="select-birthday" name="month" id="birthday">
-            <option value="--">--</option>
-            <option value="1">01</option>
-            <option value="2">02</option>
-            <option value="3">03</option>
-            <option value="4">04</option>
-            <option value="5">05</option>
-            <option value="6">06</option>
-            <option value="7">07</option>
-            <option value="8">08</option>
-            <option value="9">09</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-         </select>
+			<select class="select-year" name="year" id="birthday">
+				<option value="----">----</option>
+				<option value="2021">2021</option>
+				<option value="2020">2020</option>
+				<option value="2019">2019</option>
+				<option value="2018">2018</option>
+				<option value="2017">2017</option>
+				<option value="2016">2016</option>
+				<option value="2015">2015</option>
+				<option value="2014">2014</option>
+				<option value="2013">2013</option>
+				<option value="2012">2012</option>
+				<option value="2011">2011</option>
+				<option value="2010">2010</option>
+				<option value="2009">2009</option>
+				<option value="2008">2008</option>
+				<option value="2007">2007</option>
+				<option value="2006">2006</option>
+				<option value="2005">2005</option>
+				<option value="2004">2004</option>
+				<option value="2003">2003</option>
+				<option value="2002">2002</option>
+				<option value="2001">2001</option>
+				<option value="2000">2000</option>
+				<option value="1999">1999</option>
+				<option value="1998">1998</option>
+				<option value="1997">1997</option>
+				<option value="1996">1996</option>
+				<option value="1995">1995</option>
+				<option value="1994">1994</option>
+				<option value="1993">1993</option>
+				<option value="1992">1992</option>
+				<option value="1991">1991</option>
+				<option value="1990">1990</option>
+				<option value="1989">1989</option>
+				<option value="1988">1988</option>
+				<option value="1987">1987</option>
+				<option value="1986">1986</option>
+				<option value="1985">1985</option>
+				<option value="1984">1984</option>
+				<option value="1983">1983</option>
+				<option value="1982">1982</option>
+				<option value="1981">1981</option>
+				<option value="1980">1980</option>
+				<option value="1979">1979</option>
+				<option value="1978">1978</option>
+				<option value="1977">1977</option>
+				<option value="1976">1976</option>
+				<option value="1975">1975</option>
+				<option value="1974">1974</option>
+				<option value="1973">1973</option>
+				<option value="1972">1972</option>
+				<option value="1971">1971</option>
+				<option value="1970">1970</option>
+				<option value="1969">1969</option>
+				<option value="1968">1968</option>
+				<option value="1967">1967</option>
+				<option value="1966">1966</option>
+				<option value="1965">1965</option>
+				<option value="1964">1964</option>
+				<option value="1963">1963</option>
+				<option value="1962">1962</option>
+				<option value="1961">1961</option>
+				<option value="1960">1960</option>
+			</select>
 
-         <select class="select-year" name="year" id="birthday">
-            <option value="----">----</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-            <option value="2018">2018</option>
-            <option value="2017">2017</option>
-            <option value="2016">2016</option>
-            <option value="2015">2015</option>
-            <option value="2014">2014</option>
-            <option value="2013">2013</option>
-            <option value="2012">2012</option>
-            <option value="2011">2011</option>
-            <option value="2010">2010</option>
-            <option value="2009">2009</option>
-            <option value="2008">2008</option>
-            <option value="2007">2007</option>
-            <option value="2006">2006</option>
-            <option value="2005">2005</option>
-            <option value="2004">2004</option>
-            <option value="2003">2003</option>
-            <option value="2002">2002</option>
-            <option value="2001">2001</option>
-            <option value="2000">2000</option>
-            <option value="1999">1999</option>
-            <option value="1998">1998</option>
-            <option value="1997">1997</option>
-            <option value="1996">1996</option>
-            <option value="1995">1995</option>
-            <option value="1994">1994</option>
-            <option value="1993">1993</option>
-            <option value="1992">1992</option>
-            <option value="1991">1991</option>
-            <option value="1990">1990</option>
-            <option value="1989">1989</option>
-            <option value="1988">1988</option>
-            <option value="1987">1987</option>
-            <option value="1986">1986</option>
-            <option value="1985">1985</option>
-            <option value="1984">1984</option>
-            <option value="1983">1983</option>
-            <option value="1982">1982</option>
-            <option value="1981">1981</option>
-            <option value="1980">1980</option>
-            <option value="1979">1979</option>
-            <option value="1978">1978</option>
-            <option value="1977">1977</option>
-            <option value="1976">1976</option>
-            <option value="1975">1975</option>
-            <option value="1974">1974</option>
-            <option value="1973">1973</option>
-            <option value="1972">1972</option>
-            <option value="1971">1971</option>
-            <option value="1970">1970</option>
-            <option value="1969">1969</option>
-            <option value="1968">1968</option>
-            <option value="1967">1967</option>
-            <option value="1966">1966</option>
-            <option value="1965">1965</option>
-            <option value="1964">1964</option>
-            <option value="1963">1963</option>
-            <option value="1962">1962</option>
-            <option value="1961">1961</option>
-            <option value="1960">1960</option>
-         </select>
-
-         <input type="text" name="message" id="message" placeholder="Message" v-model="form.message" />
-
-      </fieldset>
-   </form>
+			<input
+				type="text"
+				name="message"
+				id="message"
+				placeholder="Message"
+				v-model="form.message"
+			/>
+		</fieldset>
+	</form>
 </template>
 
 <script>
-   export default {
-      data() {
-         return {
-            props: {
-               firstName:   String,
-               lastName:    String,
-               address:     String,
-               postAdress:  Number,
-               birthday:    Number,
-               phoneNumber: Number,
-               email:       String,
-               message:     String,
-            },
+export default {
+	data() {
+		return {
+			props: {
+				firstName: String,
+				lastName: String,
+				address: String,
+				postAdress: Number,
+				birthday: Number,
+				phoneNumber: Number,
+				email: String,
+				message: String,
+			},
 
-            form: {
-               firstName:   null,
-               lastName:    null,
-               address:     null,
-               postAdress:  null,
-               birthday:    null,
-               phoneNumber: null,
-               email:       null,
-               message:     null,
-            },
+			form: {
+				firstName: null,
+				lastName: null,
+				address: null,
+				postAdress: null,
+				birthday: null,
+				phoneNumber: null,
+				email: null,
+				message: null,
+			},
 
-            options: {
-               gender: ['Male', 'Female', 'Skip'],
-            },
-         }
-      }
-   };
+			options: {
+				gender: ['Male', 'Female', 'Skip'],
+			},
+		};
+	},
+};
 </script>
 
-<style>
-</style>
+<style></style>
