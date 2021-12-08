cca12eaa4752d8f33b1d426fa2b59de5c7b56463
Dines <dinesvlarsen@gmail.com>
2021-12-08 02:09:41 +0100

    added some classes to the icons

--- src/components/TodoIcons.vue
+++ src/components/TodoIcons.vue
@@ -1,10 +1,10 @@
 <template>
    <!-- mounting font icons with attribue done, add and remove for emit/events -->
-   <i class="far fa-check-circle fa-2x" v-if="font === 'done'"></i>
+   <i class="todo__done-task far fa-check-circle fa-2x" v-if="font === 'done'"></i>
 
-   <i class="fas fa-plus fa-2x" v-else-if="font === 'add'"></i>
+   <i class="todo__add-task--icon fas fa-plus fa-2x" v-else-if="font === 'add'"></i>
 
-   <i class="fas fa-minus-circle fa-2x" v-else-if="font === 'remove'"></i>
+   <i class="todo__remove-task fas fa-minus-circle fa-2x" v-else-if="font === 'remove'"></i>
 </template>
 
 <script>
